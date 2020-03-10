890a2f91e803938e87d87b00384e3392e3664b33
Samuel Wong <samuelw5@student.unimelb.edu.au>
2020-03-10 16:26:47 -0600

    Solve Problem #1

---
+++ src/solutions/solution001.py
@@ -0,0 +1,63 @@
+
+from typing import List
+
+# Good morning! Here's your coding interview problem for today.
+#
+# This problem was recently asked by Google.
+#
+# Given a list of numbers and a number k, return whether any two numbers from 
+# the list add up to k.
+#
+# For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+#
+# Bonus: Can you do this in one pass?
+
+
+# Test Format: (Tuple of inputs, Expected output)
+tests = (
+    # Given example test
+    (([10, 15, 3, 7], 17), True),
+    # should return false on the empty list
+    (([], 0), False),
+    # should return false on lists with a single element
+    (([0], 0), False),
+    (([1], 1), False),
+    (([-1], -1), False),
+    # should handle sums to zero
+    (([0, 0], 0), True),
+    (([1, -1], 0), True),
+    # should handle summing with zero
+    (([0, 1], 1), True),
+    (([1, 0], 1), True),
+    # should handle positive sums
+    (([1, 2], 3), True),
+    (([2, 1], 3), True),
+    # should handle negative sums
+    (([-1, -1], -2), True),
+    (([1, -2], -1), True),
+    # should handle list of 4 elements that match at the start
+    (([1, 3, 2, 4], 4), True),
+    # should handle list of 4 elements that match at the end
+    (([1, 3, 2, 4], 6), True),
+    # should handle list of 4 elements that match in the middle
+    (([1, 3, 2, 4], 5), True),
+    # should not match when sum to k doesn't exist in list
+    (([6, 4, 2, 0], 3), False)
+)
+
+
+def any_two_numbers_add_up_to_k(numbers: List, k: int):
+    if len(numbers) <= 1:
+        return False
+
+    target = set()
+    for number in numbers:
+        if number in target:
+            return True
+        new_target = k - number
+        target.add(new_target)
+
+    return False
+
+
+solver = any_two_numbers_add_up_to_k
