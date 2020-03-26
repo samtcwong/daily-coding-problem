5f1f5702a81ee899c00265d644599409b455febc
Sam Wong <samtcwong@gmail.com>
2020-03-26 19:55:37 +1100

    Add unsolved problems #3, #5-16

---
+++ src/solutions/solution010.py
@@ -0,0 +1,12 @@
+from typing import List
+
+# Problem #10 [Medium]
+# Good morning! Here's your coding interview problem for today.
+
+# This problem was asked by Apple.
+
+# Implement a job scheduler which takes in a function f and an integer n, and
+# calls f after n milliseconds.
+
+tests = None
+solver = None
---
+++ src/solutions/solution014.py
@@ -0,0 +1,13 @@
+from typing import List
+
+# Problem #14 [Medium]
+# Good morning! Here's your coding interview problem for today.
+
+# This problem was asked by Google.
+
+# The area of a circle is defined as πr^2. Estimate π to 3 decimal places using a Monte Carlo method.
+
+# Hint: The basic equation of a circle is x^2 + y^2 = r^2.
+
+tests = None
+solver = None
